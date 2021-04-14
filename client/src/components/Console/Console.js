41316a9666eb49bbbf80b75ef194cf652fd633c5
Creed Zagrzebski <creed@DESKTOP-4JOT5NT.localdomain>
2021-04-14 12:29:41 -0500

    Added Server State Detection and implemented Client Console

--- client/src/components/Console/Console.js
+++ client/src/components/Console/Console.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Console.css';
+
+const AlwaysScrollToBottom = () => {
+    const elementRef = useRef();
+    useEffect(() => elementRef.current.scrollIntoView());
+    return <div ref={elementRef} />;
+  };
 
 function Console({ consoleOutputList }){
 
     return (
         <div className="console">
-            {consoleOutputList.map((log, i) => {
-                return <p key={i}>{ log }</p>
+            {consoleOutputList.map((logs) => {
+                return logs.map((log, i) => {
+                    return <p key={i}>{ log }</p>
+                })
             })}
+             <AlwaysScrollToBottom />
         </div>
     )
 }
