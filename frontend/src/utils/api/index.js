d82bc6c7ff811b550767ab0e654bf47b147cf330
Creed Zagrzebski <creed@DESKTOP-4JOT5NT.localdomain>
2021-08-16 18:20:00 -0500

    Refactoring

--- frontend/src/utils/api/index.js
+++ frontend/src/utils/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const baseUrl = "http://localhost:3500/";
 
 export default axios.create({
-    baseURL: "http://localhost:3500/",
+    baseURL: baseUrl,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
